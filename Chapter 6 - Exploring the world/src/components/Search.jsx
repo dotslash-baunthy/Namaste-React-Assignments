741c22a5e35b80e3bb0bf2a50b2b0ab9fb4e4399
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2023-01-22 21:04:45 +0530

    Update search functionality

--- Chapter 6 - Exploring the world/src/components/Search.jsx
+++ Chapter 6 - Exploring the world/src/components/Search.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { RestaurantList } from "./RestaurantList.jsx";
-import { Error } from "./Error.jsx";
 import { Shimmer } from "./Shimmer.jsx";
 
 export const Search = () => {
@@ -21,27 +20,10 @@ export const Search = () => {
     }, [])
 
     async function getRestaurants() {
-        try {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5822999&lng=77.0499762&page_type=DESKTOP_WEB_LISTING");
-            const json = await data.json();
-            setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-            setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
-            if (!data.ok) {
-                if (data.status === 429) {
-                    // Rate limit exceeded
-                }
-                if (data.status === 404) {
-                    // Not found
-                }
-                if (data.status === 403) {
-                    // Not authorized
-                }
-            }
-        }
-        catch (e) {
-            // console.log(e);
-            <Error />
-        }
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5822999&lng=77.0499762&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
     }
 
     if (filteredRestaurants.length === 0) {
