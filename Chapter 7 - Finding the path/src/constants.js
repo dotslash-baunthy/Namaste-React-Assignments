698a9962b9107174d31711cc9962ba30b97c2f8a
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2023-01-27 00:16:43 +0530

    Update constants.js
    Add stub link for restaurant menu

--- Chapter 7 - Finding the path/src/constants.js
+++ Chapter 7 - Finding the path/src/constants.js
@@ -1,2 +1,3 @@
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const RESTAURANT_MENU_API = "https://www.swiggy.com/dapi/menu/v4/full?lat=28.5822999&lng=77.0499762&menuId=";
